0f9b91ee34858f205f5000e343ded5e34525b4f2
owlsogul <owlsogul@naver.com>
2017-08-29 20:01:10 +0900

    error fix: change app to router

--- webcoder/router.js
+++ webcoder/router.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-app.use(express.static(__dirname + '/public'));
+router.use(express.static(__dirname + '/public'));
 
 // define the home page route
 router.get('/', function(req, res) {
